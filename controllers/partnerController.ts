dec0b6f552820d96072ce429ca25c60c9b73082c
thuanho95 <thuan_ho@helloleotech.com>
2021-01-07 15:08:46 +0700

    first commit

--- controllers/partnerController.ts
+++ controllers/partnerController.ts
@@ -14,7 +14,7 @@ export class PartnerController {
             req.body.email &&
             req.body.phone_number &&
             req.body.gender) {
-            const user_params: IPartner = {
+            const partnerParams: IPartner = {
                 name: {
                     first_name: req.body.name.first_name,
                     middle_name: req.body.name.middle_name,
@@ -26,94 +26,18 @@ export class PartnerController {
                 modification_notes: [{
                     modified_on: new Date(Date.now()),
                     modified_by: null,
-                    modification_note: 'New user created'
+                    modification_note: 'New partner created'
                 }]
             };
-            this.partnerService.createUser(user_params, (err: any, user_data: IPartner) => {
+            this.partnerService.createPartner(partnerParams, (err: any, partnerData: IPartner) => {
                 if (err) {
                     mongoError(err, res);
                 } else {
-                    successResponse('create user successfull', user_data, res);
+                    successResponse('create partner successfull', partnerData, res);
                 }
             });
         } else {
             // error response if some fields are missing in request body
-            insufficientParameters(res);
-        }
-    }
-
-    public getPartner(req: Request, res: Response) {
-        if (req.params.id) {
-            const user_filter = { _id: req.params.id };
-            this.partnerService.filterUser(user_filter, (err: any, user_data: IPartner) => {
-                if (err) {
-                    mongoError(err, res);
-                } else {
-                    successResponse('get user successfull', user_data, res);
-                }
-            });
-        } else {
-            insufficientParameters(res);
-        }
-    }
-
-    public updatePartner(req: Request, res: Response) {
-        if (req.params.id &&
-            req.body.name || req.body.name.first_name || req.body.name.middle_name || req.body.name.last_name ||
-            req.body.email ||
-            req.body.phone_number ||
-            req.body.gender) {
-            const user_filter = { _id: req.params.id };
-            this.partnerService.filterUser(user_filter, (err: any, user_data: IPartner) => {
-                if (err) {
-                    mongoError(err, res);
-                } else if (user_data) {
-                    user_data.modification_notes.push({
-                        modified_on: new Date(Date.now()),
-                        modified_by: null,
-                        modification_note: 'User data updated'
-                    });
-                    const user_params: IPartner = {
-                        _id: req.params.id,
-                        name: req.body.name ? {
-                            first_name: req.body.name.first_name ? req.body.name.first_name : user_data.name.first_name,
-                            middle_name: req.body.name.first_name ? req.body.name.middle_name : user_data.name.middle_name,
-                            last_name: req.body.name.first_name ? req.body.name.last_name : user_data.name.last_name
-                        } : user_data.name,
-                        email: req.body.email ? req.body.email : user_data.email,
-                        phone_number: req.body.phone_number ? req.body.phone_number : user_data.phone_number,
-                        gender: req.body.gender ? req.body.gender : user_data.gender,
-                        is_deleted: req.body.is_deleted ? req.body.is_deleted : user_data.is_deleted,
-                        modification_notes: user_data.modification_notes
-                    };
-                    this.partnerService.updateUser(user_params, (err: any) => {
-                        if (err) {
-                            mongoError(err, res);
-                        } else {
-                            successResponse('update user successfull', null, res);
-                        }
-                    });
-                } else {
-                    failureResponse('invalid user', null, res);
-                }
-            });
-        } else {
-            insufficientParameters(res);
-        }
-    }
-
-    public deletePartner(req: Request, res: Response) {
-        if (req.params.id) {
-            this.partnerService.deleteUser(req.params.id, (err: any, delete_details) => {
-                if (err) {
-                    mongoError(err, res);
-                } else if (delete_details.deletedCount !== 0) {
-                    successResponse('delete user successfull', null, res);
-                } else {
-                    failureResponse('invalid user', null, res);
-                }
-            });
-        } else {
             insufficientParameters(res);
         }
     }
