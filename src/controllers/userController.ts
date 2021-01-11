9801a415051c2a0b63f31f2af332c14bf656e3b7
Leo Tech <helloleotech@gmail.com>
2021-01-11 08:42:27 +0700

    Fixed messenger userController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -20,14 +20,14 @@ export class UserController {
             companyName,
             companyRole,} = req.body;
         if(!(name && telephone && mobile && email && password && dateOfBirth && companyName  )){
-            return failureResponse("all fill is requied", null, res);
+            return failureResponse("All fill is requied", null, res);
         }
         this.userService.filterUser({email},(err: Error, user: IUser) =>{
             if(err){
                 return mongoError(err, res);
             }
             if(user){
-                return failureResponse("email is already use", null, res);
+                return failureResponse("Email is already use", null, res);
             }
         })
         const userParams: IUser = { 
@@ -43,14 +43,14 @@ export class UserController {
             modificationNotes: [{
                 modifiedBy: null,
                 modifiedOn: new Date(),
-                modificationNote: 'create new user',
+                modificationNote: 'Create new user',
             }]
         }
         this.userService.createUser(userParams, (err: Error, newUser: IUser) =>{
             if(err){
                 return mongoError(err, res);
             }
-            return successResponse("create user successful", newUser, res);
+            return successResponse("Create user successful", newUser, res);
         })
     }
 }
