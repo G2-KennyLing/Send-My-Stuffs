583f4848e915bfe8ce7ba4744f8f09415b1cb663
Leo Tech <helloleotech@gmail.com>
2021-01-14 14:19:29 +0700

    Fixed bugs Users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -11,6 +11,7 @@ export class UsersRoutes {
 
         const isSignIn = this.authController.isSignIn;
         const isAdmin = this.authController.isAdmin;
+        const isSuperAdmin = this.authController.isSuperAdmin;
         
         app.post('/user', isSignIn, (req: Request, res: Response) => {
             this.userController.createUser(req, res);
