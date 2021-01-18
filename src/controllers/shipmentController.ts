32dfcc25cd4903eb2aaf2ed99c84821823fc7aac
thuanho95 <thuan_ho@helloleotech.com>
2021-01-18 20:25:04 +0700

    update shipment

--- src/controllers/shipmentController.ts
+++ src/controllers/shipmentController.ts
@@ -1,74 +1,71 @@
 import { ModificationNote } from "./../modules/common/model";
 import { Request, Response } from "express";
-import {insufficientParameters,mongoError, successResponse,failureResponse,
+import {
+  insufficientParameters, mongoError, successResponse, failureResponse,
 } from "../modules/common/service";
 import ShipmentService from "../modules/shipment/service";
 import IShipment from "../modules/shipment/model";
 export default class ShipmentController {
 
-  private Service: ShipmentService;
-  constructor() {
-    this.Service = new ShipmentService();
-  }
+  private shipmentService: ShipmentService = new ShipmentService();
 
   public createShipment(req: Request, res: Response) {
-    const {shipmentNo,from,to,vesselName,voyage,cargoDescription,departureDate,landingDate,} = req.body;
+    const { shipmentNo, from, to, vesselName, voyage, cargoDescription, departureDate, landingDate, } = req.body;
     if (!(shipmentNo && from && to && cargoDescription && departureDate && landingDate))
       return insufficientParameters(res);
 
     if (!((vesselName && !voyage) || (!vesselName && voyage)))
-      return failureResponse("Can not choose both transport method",{ voyage, vesselName },res);
+      return failureResponse("Can not choose both transport method", { voyage, vesselName }, res);
 
-    this.Service.create(
-      {shipmentNo,from,to,vesselName,voyage,cargoDescription,departureDate,landingDate,},
-      (err, newShipment) => {
-        if (err) return mongoError(err, res);
-        return successResponse("Create new shipment successful",newShipment, res
-        );
-      }
-    );
-  }
-  
-  public getListShipments(req: Request, res: Response) {
-    this.Service.filterShipments((error, shipments) => {
-      if (error) return mongoError(error, res);
-      return successResponse("Get all shipments successfull", shipments, res);
-    });
-  }
-
-  public getShipment(req: Request, res: Response) {
-    const { _id } = req.params;
-    this.Service.filterShipment(_id, (err: Error, shipment: IShipment) => {
-      if (err) return mongoError(err, res);
-      if (!shipment) return failureResponse("Shipment is not found", {}, res);
-      return successResponse("Get shipment successful", shipment, res);
-    });
-  }
-
-  public updateShipment(req: Request, res: Response) {
-    const { _id } = req.params;
-    const {
+    const shipmentParams: IShipment = {
       shipmentNo,
       from,
       to,
       vesselName,
       voyage,
       cargoDescription,
-      status,
       departureDate,
       landingDate,
-    } = req.body;
-    if (
-      !(
-        shipmentNo &&
-        from &&
-        to &&
-        cargoDescription &&
-        departureDate &&
-        landingDate &&
-        status !== undefined
-      )
-    )
+      ModificationNote: [{
+        modifiedOn: new Date(Date.now()),
+        modifiedBy: null,
+        modificationNote: 'New partner created'
+      }]
+    };
+    this.shipmentService.createShipment(shipmentParams, (err: any, shipmentData: IShipment) => {
+      if (err) {
+        mongoError(err, res);
+      } else {
+        successResponse('Create shipment successfull', shipmentData, res);
+      }
+    });
+  }
+
+  public getListShipments(req: Request, res: Response) {
+    const shipmentFilter = {};
+    this.shipmentService.filterShipments(shipmentFilter, (err: any, shipmentData: IShipment) => {
+      if (err) {
+        mongoError(err, res);
+      } else {
+        successResponse("Get list shipments successful", shipmentData, res);
+      }
+    });
+  }
+
+  public getShipment(req: Request, res: Response) {
+    const shipmentFilter = { _id: req.params.id };
+    this.shipmentService.filterShipment(shipmentFilter, (err: any, shipmentData: IShipment) => {
+      if (err) {
+        mongoError(err, res);
+      } else {
+        successResponse("Get shipment detail successful", shipmentData, res);
+      }
+    });
+  }
+
+  public updateShipment(req: Request, res: Response) {
+    const { shipmentNo, from, to, vesselName, voyage, cargoDescription, status, departureDate, landingDate, } = req.body;
+    if (!(shipmentNo && from && to && cargoDescription && departureDate && landingDate && status !== undefined))
       return insufficientParameters(res);
     if (!((vesselName && !voyage) || (!vesselName && voyage)))
       return failureResponse(
@@ -76,47 +73,44 @@ export default class ShipmentController {
         { voyage, vesselName },
         res
       );
-    const ModificationNote: ModificationNote = {
-      modificationNote: "update shipment",
-      modifiedOn: new Date(),
-      modifiedBy: "",
-    };
-    const updatedShipment: IShipment = {
-      shipmentNo,
-      from,
-      to,
-      vesselName,
-      voyage,
-      cargoDescription,
-      status,
-      departureDate,
-      landingDate,
-    };
-    if (!_id) return insufficientParameters(res);
-    this.Service.updateShipment(
-      _id,
-      updatedShipment,
-      ModificationNote,
-      (error, shipment) => {
-        if (error) return mongoError(error, res);
-        if (!shipment)
-          return failureResponse(
-            "Shipment with that ID is not exist",
-            shipment,
-            res
-          );
-        return successResponse(
-          `Update shipment${shipment._id} successful`,
-          shipment,
-          res
-        );
+    const shipmentFilter = { _id: req.params.id };
+    this.shipmentService.filterShipment(shipmentFilter, (err: any, shipmentData: IShipment) => {
+      if (err) {
+        return mongoError(err, res);
       }
-    );
+      if (shipmentData) {
+        const shipmentParams: IShipment = {
+          _id: req.params.id,
+          shipmentNo: shipmentNo ? req.body.shipmentNo : shipmentData.shipmentNo,
+          from: from ? req.body.from : shipmentData.from,
+          to: to ? req.body.to : shipmentData.to,
+          vesselName: vesselName ? req.body.vesselName : shipmentData.vesselName,
+          voyage: voyage ? req.body.voyage : shipmentData.voyage,
+          cargoDescription: cargoDescription ? req.body.cargoDescription : shipmentData.cargoDescription,
+          departureDate: departureDate ? req.body.depatureDate : shipmentData.departureDate,
+          landingDate: landingDate ? req.body.landingDate : shipmentData.landingDate,
+          ModificationNote: [{
+            modifiedOn: new Date(Date.now()),
+            modifiedBy: null,
+            modificationNote: 'Shipment data updated'
+          }]
+        }
+        this.shipmentService.updateShipment(shipmentParams, (err: any) => {
+          if (err) {
+            mongoError(err, res);
+          } else {
+            successResponse("Update shipment successful", shipmentParams, res);
+          }
+        });
+      } else {
+        failureResponse("Invalid shipment", null, res);
+      }
+    });
   }
 
   async overviewShipment(req: Request, res: Response) {
-    const departure = await this.Service.getOverviewDepature();
-    const landing = await this.Service.getOverviewLanding();
+    const departure = await this.shipmentService.getOverviewDepature();
+    const landing = await this.shipmentService.getOverviewLanding();
     const response = {
       departure,
       landing,
