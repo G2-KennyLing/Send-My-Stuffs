2fe5d7c236736f00ea2a8e0c89fa87fb41254d07
quocdatleotech <dat_nguyen@helloleotech.com>
2021-01-25 16:47:14 +0700

    done populate relationship state

--- src/routes/state.ts
+++ src/routes/state.ts
@@ -1,27 +1,31 @@
 import { Application, Request, Response } from "express";
 import { StateController } from '../controllers/statesController';
+import { AuthController } from '../controllers/authController';
 
 export class StateRoutes {
 	private statesController: StateController = new StateController();
+	private authController: AuthController = new AuthController();
 
 	public route(app: Application) {
-		app.post('/state', (req: Request, res: Response) => {
+		const isSignIn = this.authController.isSignIn;
+
+		app.post('/state', isSignIn, (req: Request, res: Response) => {
 			this.statesController.createStates(req, res);
 		})
 
-		app.get('/states', (req: Request, res: Response) => {
+		app.get('/states', isSignIn, (req: Request, res: Response) => {
 			this.statesController.getListStates(req, res)
 		})
 
-		app.get('/state/:id', (req: Request, res: Response) => {
+		app.get('/state/:id', isSignIn, (req: Request, res: Response) => {
 			this.statesController.getState(req, res)
 		})
 
-		app.put('/state/:id', (req: Request, res: Response) => {
+		app.put('/state/:id', isSignIn, (req: Request, res: Response) => {
 			this.statesController.updateState(req, res)
 		})
 
-		app.delete('/state/:id', (req: Request, res: Response) => {
+		app.delete('/state/:id', isSignIn, (req: Request, res: Response) => {
 			this.statesController.isDelete(req, res)
 		})
 	}
