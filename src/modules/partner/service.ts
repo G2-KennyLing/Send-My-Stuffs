f1c0d12e637a8e540e038737d50df80bdd738290
thuanho95 <thuan_ho@helloleotech.com>
2021-02-02 14:31:20 +0700

    Fix get list country

--- src/modules/partner/service.ts
+++ src/modules/partner/service.ts
@@ -11,7 +11,7 @@ export default class PartnerService {
     public filterPartners(param: any,query: any, callback: any) {
         const limit =  param.limit;
         const page = param.page;
-        return Partner.find(query, callback).populate('country', 'countryName').populate('city').populate('salesID').limit(limit * 1 ).skip((page - 1) * limit);;
+        Partner.find(query, callback).populate('country', 'countryName').populate('city').populate('salesID').limit(limit * 1 ).skip((page - 1) * limit);;
     }
 
     public filterPartner(query: any, callback: any) {
