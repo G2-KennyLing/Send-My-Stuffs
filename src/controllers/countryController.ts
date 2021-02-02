515a5b4bf1d724d63bca489d806c80c09e6eda00
thuanho95 <thuan_ho@helloleotech.com>
2021-02-02 15:18:27 +0700

    Fix get list country

--- src/controllers/countryController.ts
+++ src/controllers/countryController.ts
@@ -46,14 +46,14 @@ export class CountryController {
 	}
 
 	public getListCountries(req: Request, res: Response) {
-		const countryFilter = {};
-		this.countryService.filterCountries(countryFilter, (err: any, countryData: ICountry) => {
-			if (err) {
-				return mongoError(err, res);
-			}
-			return successResponse("Get list countries successful", countryData, res)
-		})
-	}
+        const countryFilter = {};
+        this.countryService.filterCountries(countryFilter, (err: any, countryData: ICountry) => {
+            if (err) {
+               return mongoError(err, res);
+            }
+            return successResponse("Get list country successful", countryData, res);
+        })
+    }
 
 	public getCountry(req: Request, res: Response) {
 		const detailCountryId = { _id: req.params.id };
--- src/modules/country/service.ts
+++ src/modules/country/service.ts
@@ -1,24 +1,24 @@
 import { ICountry } from './model';
-import Country  from './schema';
+import Countries  from './schema';
 
 export default class CountryService {
 
 	public createCountry(countryParams: ICountry, callback:any) {
-		const _session = new Country(countryParams);
+		const _session = new Countries(countryParams);
 		_session.save(callback);
 	}
 
 	public filterCountries(query: any, callback: any) {
-		Country.find(query, callback).populate('seaPorts', 'seaportName').populate('airPorts', 'airportName');
+		Countries.find(query, callback).populate('seaPorts', 'seaportName').populate('airPorts', 'airportName');
 	}
 
 	public filterCountry(query: any, callback: any) {
-		Country.findOne(query, callback).populate('seaPorts', 'seaportName').populate('airPorts', 'airportName')
+		Countries.findOne(query, callback).populate('seaPorts', 'seaportName').populate('airPorts', 'airportName')
 	}
 
 	public updateCountry(countryParams: ICountry, callback: any) {
 		const query = { _id: countryParams._id };
-		Country.findOneAndUpdate(query, countryParams, callback)
+		Countries.findOneAndUpdate(query, countryParams, callback)
 	}
 
 }
